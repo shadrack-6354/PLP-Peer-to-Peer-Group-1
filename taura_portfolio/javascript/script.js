ac8972e5e9a752709ba07a334efe3aaa8d79081f
Taura Anderson <tauraanderson56@gmail.com>
2024-11-25 17:37:25 +0300

    Add files via upload

---
+++ taura_portfolio/javascript/script.js
@@ -0,0 +1,10 @@
+// Handle responsive navigation toggle
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Load Feather icons
+feather.replace();
